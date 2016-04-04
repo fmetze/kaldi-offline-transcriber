cce6be1845a189aa026983295dd3787e4805c1c3
Florian Metze <fmetze@cs.cmu.edu>
2016-04-04 18:13:22 -0400

    make code more robust

--- scripts/segmented-ctm-to-hyp.py
+++ scripts/segmented-ctm-to-hyp.py
@@ -7,8 +7,11 @@ for l in sys.stdin:
   if not l.startswith("#"):
     ss = l.split()
     sent_id = ss[0]
-    word = ss[4]
-    sentences[sent_id] = sentences.get(sent_id, []) + [word]
+    try:
+      word = ss[4]
+      sentences[sent_id] = sentences.get(sent_id, []) + [word]
+    except:
+      pass
 
 for sent_id in sorted(sentences):
   print "%s (%s)" % (" ".join(sentences[sent_id]), sent_id)
